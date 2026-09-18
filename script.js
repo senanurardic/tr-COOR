2325db2557f6a0c82a51c00610f5c08f4cc5cc4d
senanurardic <senabacaksiz@gmail.com>
2026-09-18 15:08:08 +0200

    ilk commit

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const CONDITION = "COOR";
 const CONDITION_LABEL = "Coor Condition";
 
 // Map & Camera Settings
-const MAP_CENTER = [32.888932, 39.929227];
+const MAP_CENTER = [32.889145, 39.929722];
 const SCENE_ROTATION_DEG = 41;
 function rot(bearingDeg) { return (bearingDeg + SCENE_ROTATION_DEG + 360) % 360; }
-const MAP_ZOOM = 17.0;
+const MAP_ZOOM = 16.5;
 
 const WALK_SPEED_MPS = 4.5;
 
